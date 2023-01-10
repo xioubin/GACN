de7fcf91642c99cdd30a2ce53cff276e581f3841
Yubin <b10830038@gapps.ntust.edu.tw>
2023-01-10 15:18:46 +0800

    update compare

--- compare.py
+++ compare.py
@@ -3,11 +3,23 @@
 
 from PIL import Image
 import numpy as np
-import os
-img1 = np.array(Image.open("color_lytro_06_new.png"))
-img2 = np.array(Image.open("data/result/color_lytro_06.png"))
-np.set_printoptions(threshold=np.inf, linewidth=520, precision=0)
-diff = img1[:,:,:3] - img2
-diff = diff.sum(axis=2)
+from skimage.metrics import structural_similarity as ssim
+import cv2
 
-print(diff)
+img1 = cv2.imread("color_lytro_20_new.png")
+img2 = cv2.imread("data/result/color_lytro_20.png")
+
+# PSNR
+
+# MSE = cv2.mse(img1, img2)
+PSNR_self = cv2.PSNR(img2, img2)
+PSNR = cv2.PSNR(img1, img2)
+
+# SSIM
+
+SSIM = ssim(img1, img2, multichannel=True)
+
+# print("MSE: ", MSE)
+print("PSMR_self: ", PSNR_self)
+print("PSNR: ", PSNR)
+print("SSIM: ", SSIM)
